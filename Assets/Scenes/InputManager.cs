49a32bad25954d3bb4492c0e88337ae16e0fcea1
wqferr <wqferr@gmail.com>
2018-08-10 23:36:37 -0300

    Create dummy input manager

---
+++ Assets/Scenes/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public enum ControllerButton
+    {
+        JUMP,
+        ACTION
+    }
+
+    public float GetHorizontalAxis(int player)
+    {
+        return 0;
+    }
+
+    public bool GetButton(int player, ControllerButton button)
+    {
+        return false;
+    }
+}
